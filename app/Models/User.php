95e0ad691268c68d1ae9acd42c4126a0ca56d711
ShoContain <perblue.666@gmail.com>
2023-06-20 19:19:25 +0900

    Userモデル作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Role;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles() : BelongsToMany
+    {
+        return $this
+            ->belongsToMany(Role::class);
+    }
 }
